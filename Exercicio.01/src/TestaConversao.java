7d74ee44f7b98020ca51a1ec7c1f7b4de4992577
lucas <lucasalipio@gmail.com>
2019-08-19 21:55:34 -0300

    Comando de conversão

---
+++ Exercicio.01/src/TestaConversao.java
@@ -0,0 +1,21 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		int idade = Integer.parseInt("30");
+		int filhos = (int) 4.2;
+		
+		short ValorPequeno = 2131;
+		byte b = 127;
+		
+		System.out.println(" Quantidade de filhos : " + filhos);
+		
+		double salario = 1250.70;
+		System.out.printf("Salario: %.2f" , salario);
+		
+		
+		
+		
+	}
+
+}
