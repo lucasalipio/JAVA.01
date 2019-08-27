50c585041fa3d5696b90ad4c2d0488ad35016de2
lucas <lucasalipio21@gmail.com>
2019-08-26 21:48:55 -0300

    Conversão.2

---
+++ Exercicio.01/src/TestaCondicional2.java
@@ -0,0 +1,16 @@
+
+public class TestaCondicional2 {
+
+	public static void main(String[] args) {
+		
+		int idade = 18;
+		int qtdpessoas = 1;
+		
+		if (idade >= 18 || qtdpessoas >= 3) {
+			System.out.println("Seja Bem-Vindo");
+		} else{
+		System.out.println("NÃO LIBERADO");
+	}
+
+}
+}
